1631f7cb135731516482ad234cd67654ce689019
TlotlegoKgatitsoe <61731529+TlotlegoKgatitsoe@users.noreply.github.com>
2020-07-13 13:33:52 +0200

    Updated server.js

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const bodyParser = require( 'body-parser' );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( bodyParser.json() );
 
+if ( process.env.NODE_ENV === 'production' ) {
+    app.use( express.static( path.join( __dirname, 'frontend/build' ) ) );
+    app.get( '*', ( req, res ) => {
+        res.sendFile( path.resolve( __dirname, 'frontend', 'build', 'index.html' ) );
+    });
+}
+
 // Will be used in the test for my backend server
 app.get( '/', ( request, response ) => {
     response.send( 'okay...' );
